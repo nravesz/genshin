77d868a145411dbf0c524ca5a885e3e22ffa84f9
nravesz <nraveszani@gmail.com>
2023-06-04 20:36:10 -0300

    feature (db): db connected

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,13 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const mongodbUrl: string | undefined = process.env.MONGODB_URL;
+export const client = new MongoClient(mongodbUrl as string);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.get('/characters', async (req: Request, res: Response) => {
+  const charactersC = client.db('UsersDB').collection('Characters');
+  const publications = await charactersC.find({}).toArray();
+  res.send(publications);
 });
 
 app.listen(port, () => {
