06c5dcb88a0d43fc247ecb9004af1fdaad0d2265
nravesz <nraveszani@gmail.com>
2023-06-09 16:22:39 -0300

    feature (CharacterRoster.ts): now you can get a list of all the characters

---
+++ back/data/elements.ts
@@ -0,0 +1,37 @@
+export const elements = {
+  geo: {
+    id: 'geo',
+    name: 'Geo',
+    simpleName: 'Stone',
+  },
+  anemo: {
+    id: 'anemo',
+    name: 'Anemo',
+    simpleName: 'Wind',
+  },
+  pyro: {
+    id: 'pyro',
+    name: 'Pyro',
+    simpleName: 'Fire',
+  },
+  hydro: {
+    id: 'hydro',
+    name: 'Hydro',
+    simpleName: 'Water',
+  },
+  electro: {
+    id: 'electro',
+    name: 'Electro',
+    simpleName: 'Electro',
+  },
+  dendro: {
+    id: 'dendro',
+    name: 'Dendro',
+    simpleName: 'Grass',
+  },
+  cryo: {
+    id: 'cryo',
+    name: 'Cryo',
+    simpleName: 'Ice',
+  },
+};
--- back/src/index.ts
+++ back/src/index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
 
+import CharacterRoster from './characters/model/CharacterRoster';
+
 dotenv.config();
 
 const app: Express = express();
@@ -14,9 +16,21 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/characters', async (req: Request, res: Response) => {
-  const charactersC = client.db('UsersDB').collection('Characters');
-  const publications = await charactersC.find({}).toArray();
-  res.send(publications);
+  // const charactersC = client.db('UsersDB').collection('Characters');
+  // const publications = await charactersC.find({}).toArray();
+  // res.send(publications);
+  const characterRoster = new CharacterRoster();
+  characterRoster.addCharacters();
+  res.send(characterRoster.getCharactersBasicInfo());
+});
+
+app.get('/uwu', async (req: Request, res: Response) => {
+  // const charactersC = client.db('UsersDB').collection('Characters');
+  // const publications = await charactersC.find({}).toArray();
+  // res.send(publications);
+  const characterRoster = new CharacterRoster();
+  characterRoster.addCharacters();
+  console.log("aaah");
 });
 
 app.listen(port, () => {
