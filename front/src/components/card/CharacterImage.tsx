83979af96619d871a31a759eff92e50cb16a2f66
nravesz <nraveszani@gmail.com>
2023-09-22 14:38:47 -0300

    visual (Card, CharacterImage): the avatar is a circle and the card has colors

--- front/src/components/card/CharacterImage.tsx
+++ front/src/components/card/CharacterImage.tsx
@@ -1,5 +1,6 @@
 import { Image } from "react-bootstrap"
 import { CHARACTER_IMAGE_URL } from "../../config";
+import "./styles/CharacterImage.scss";
 
 type Props = {
     id: string;
@@ -8,6 +9,7 @@ type Props = {
 const CharacterImage = ({ id }: Props) => {
     return (
         <Image
+            className="image"
             src={`${CHARACTER_IMAGE_URL}/${id}.png`}
         />
     );
