7c0ed6c506aba2d798043f6f5cfc45af84539d97
nravesz <nraveszani@gmail.com>
2023-09-29 18:34:24 -0300

    style (CHaracterCard Roster)

--- front/src/components/roster/CharacterCard.tsx
+++ front/src/components/roster/CharacterCard.tsx
@@ -1,6 +1,7 @@
 import Image from "react-bootstrap/esm/Image";
 import { CHARACTER_IMAGE_URL } from "../../config";
 import './styles/CharacterCards.scss';
+import '../card/styles/Card.scss';
 
 type Props = {
     id: string;
@@ -13,9 +14,12 @@ const CharacterCard = ({ id, name } : Props) => {
             className="character-card"
         >
             <Image
+                className="image"
                 src={`${CHARACTER_IMAGE_URL}/${id}.png`}
             />
-            <p>{name}</p>
+            <p
+                className="text"
+            >{name}</p>
         </div>
     );
 };
