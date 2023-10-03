7d797e6b11202bc26019f4985b77b9ec2229a51b
nravesz <nraveszani@gmail.com>
2023-10-03 16:28:51 -0300

    style (LevelDropdown): now it looks pretty <3

--- front/src/components/editor/LevelDropdown.tsx
+++ front/src/components/editor/LevelDropdown.tsx
@@ -16,54 +16,56 @@ const LevelDropdown = ({ LvL, setLvL }: Props) => {
                 {LvL.LvL} {LvL.isAscended ? "*" : ""}
             </Dropdown.Toggle>
             <Dropdown.Menu className="dropdown-menu">
-                {Array.from({ length: 8 }).map((_, index) => (
-                    (index === 0) ? (
-                        <Row key={index}>
-                            <Col className="dropdown-col-top" >
-                                <Dropdown.Item className="dropdown-item" >
-                                    <LevelDropdownItem
-                                        LvL={index + 1}
-                                        isAscended={false}
-                                        setLvL={setLvL}
-                                    />
-                                </Dropdown.Item>
-                            </Col>
-                        </Row>
-                    ) : (index === 7) ? (
-                        <Row key={index}>
-                            <Col className="dropdown-col-bottom" >
-                                <Dropdown.Item className="dropdown-item" >
-                                    <LevelDropdownItem
-                                        LvL={(index + 1) * 10}
-                                        isAscended={false}
-                                        setLvL={setLvL}
-                                    />
-                                </Dropdown.Item>
-                            </Col>
-                        </Row>
-                    ) : (
-                        <Row className="dropdown-row" key={index}>
-                            <Col className="dropdown-col-left" >
-                                <Dropdown.Item className="dropdown-item" >
-                                    <LevelDropdownItem
-                                        LvL={(index + 1) * 10}
-                                        isAscended={false}
-                                        setLvL={setLvL}
-                                    />
-                                </Dropdown.Item>
-                            </Col>
-                            <Col className="dropdown-col-right" >
-                                <Dropdown.Item className="dropdown-item" >
-                                    <LevelDropdownItem
-                                        LvL={(index + 1) * 10}
-                                        isAscended={true}
-                                        setLvL={setLvL}
-                                    />
-                                </Dropdown.Item>
-                            </Col>
-                        </Row>
-                    )
-                ))}
+                <div className="dropdown-menu-div">
+                    {Array.from({ length: 8 }).map((_, index) => (
+                        (index === 0) ? (
+                            <Row className="dropdown-row" key={index}>
+                                <Col className="dropdown-col-top" >
+                                    <Dropdown.Item className="dropdown-item" >
+                                        <LevelDropdownItem
+                                            LvL={index + 1}
+                                            isAscended={false}
+                                            setLvL={setLvL}
+                                        />
+                                    </Dropdown.Item>
+                                </Col>
+                            </Row>
+                        ) : (index === 7) ? (
+                            <Row className="dropdown-row" key={index}>
+                                <Col className="dropdown-col-bottom" >
+                                    <Dropdown.Item className="dropdown-item" >
+                                        <LevelDropdownItem
+                                            LvL={(index + 1) * 10}
+                                            isAscended={false}
+                                            setLvL={setLvL}
+                                        />
+                                    </Dropdown.Item>
+                                </Col>
+                            </Row>
+                        ) : (
+                            <Row className="dropdown-row" key={index}>
+                                <Col className="dropdown-col-left" >
+                                    <Dropdown.Item className="dropdown-item" >
+                                        <LevelDropdownItem
+                                            LvL={(index + 1) * 10}
+                                            isAscended={false}
+                                            setLvL={setLvL}
+                                        />
+                                    </Dropdown.Item>
+                                </Col>
+                                <Col className="dropdown-col-right" >
+                                    <Dropdown.Item className="dropdown-item" >
+                                        <LevelDropdownItem
+                                            LvL={(index + 1) * 10}
+                                            isAscended={true}
+                                            setLvL={setLvL}
+                                        />
+                                    </Dropdown.Item>
+                                </Col>
+                            </Row>
+                        )
+                    ))}
+                </div>
             </Dropdown.Menu>
         </Dropdown>
     );
